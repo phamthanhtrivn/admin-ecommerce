7dbf132a8a2c3a65121b9dd30aa640701d29ee12
Tri Pham <phamthanhtri0712@gmail.com>
2025-02-27 14:24:00 +0700

    add product page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
-import { assets } from "../assets/admin_assets/assets"
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import { assets } from "../assets/admin_assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
+
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <img className="w-[max(10%,80px)]" src={assets.logo} alt="logo" />
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-sm sm:text-sm">Logout</button>
+      <Link to="/">
+        <img className="w-[max(10%,80px)]" src={assets.logo} alt="logo" />
+      </Link>
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-sm sm:text-sm"
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/layout/LayoutDefault.jsx
+++ src/layout/LayoutDefault.jsx
@@ -1,22 +1,42 @@
-import Navbar from "../components/Navbar"
-import Sidebar from "../components/Sidebar"
-import { Outlet } from "react-router-dom"
+/* eslint-disable react-refresh/only-export-components */
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
+import Login from "../components/Login";
+import { ToastContainer } from 'react-toastify';
+import { TokenContext } from "../context/TokenContext"
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const LayoutDefault = () => {
-  return (
-    <div className="bg-gray-50 min-h-screen">
-      <>
-        <Navbar />
-        <hr className="text-gray-300" />
-        <div className="flex w-full">
-          <Sidebar />
-          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-          <Outlet />
-          </div>
-        </div>
-      </>
-    </div>
-  )
-}
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
 
-export default LayoutDefault
+  useEffect(() => {
+    localStorage.setItem('token', token)
+  }, [token])
+
+  return (
+    <TokenContext.Provider value={{token}}>
+      <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr className="text-gray-300" />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Outlet/>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+    </TokenContext.Provider>
+  );
+};
+
+export default LayoutDefault;
